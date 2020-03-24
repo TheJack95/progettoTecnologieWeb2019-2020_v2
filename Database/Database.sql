7cc28c1e19cd48b567e1f14506260d1710fce11d
Erik Tabacchi <tabacchi.erik@gmail.com>
2020-03-24 04:19:24 +0100

    Add home.html, contatti.html and database.sql

---
+++ Database/Database.sql
@@ -0,0 +1,82 @@
+CREATE TABLE IF NOT EXISTS Utenti(
+    Email varchar(50),
+    Password varchar(50),
+    Nome varchar(30),
+    Cognome varchar(30),
+    Telefono varchar(15),
+    Indirizzo varchar(50),
+    DataNascita date,
+    FlAdmin enum ('0','1','2','3'),
+    PRIMARY KEY(Email)
+);
+
+CREATE TABLE IF NOT EXISTS AutoVendita(
+    Targa varchar(7),
+	Marca varchar(20),
+	Modello varchar(50),
+	KM int,
+	Cilindrata smallint,
+	PrezzoVendita int,
+	PRIMARY KEY(Targa)
+);
+
+CREATE TABLE IF NOT EXISTS AutoNoleggio(
+    Targa varchar(7),
+	Marca varchar(20),
+	Modello varchar(50),
+	Cilindrata smallint,
+	CostoNoleggio smallint,
+	Cauzione int,
+	PRIMARY KEY(Targa)
+);
+
+CREATE TABLE IF NOT EXISTS PreventivoAcquisto(
+	IdPrev int AUTO_INCREMENT,
+	Utente varchar(50),
+    Targa varchar(7),
+	PrezzoVendita int,
+	PRIMARY KEY(IdPrev)
+);
+
+CREATE TABLE IF NOT EXISTS PrenotazioneNoleggio(
+	IdPrenot int AUTO_INCREMENT,
+	Utente varchar(50),
+    Targa varchar(7),
+	InizioNoleggio date,
+	FineNoleggio date,
+	CostoTotale int,
+	PRIMARY KEY(IdPrenot)
+);
+
+CREATE TABLE IF NOT EXISTS Messaggi (
+    IdMess int AUTO_INCREMENT,
+    Nome varchar(30),
+    Cognome varchar(30),
+    Email varchar(50),
+    NumeroTelefono varchar(10),
+    Messaggio text,
+    PRIMARY KEY(IdMess)
+);
+
+ALTER TABLE PreventivoAcquisto
+	ADD CONSTRAINT FK_PrevUtente FOREIGN KEY (Utente) REFERENCES Utenti(Email)
+	ON UPDATE CASCADE;
+
+ALTER TABLE PreventivoAcquisto
+	ADD CONSTRAINT FK_PrevTarga FOREIGN KEY (Targa) REFERENCES AutoVendita(Targa);
+
+ALTER TABLE PrenotazioneNoleggio
+	ADD CONSTRAINT FK_PrenUtente FOREIGN KEY (Utente) REFERENCES Utenti(Email)
+	ON UPDATE CASCADE;
+
+ALTER TABLE PrenotazioneNoleggio
+	ADD CONSTRAINT FK_PrenTarga FOREIGN KEY (Targa) REFERENCES AutoNoleggio(Targa);
+
+
+
+
+		
+
+
+		
+		
