5438002c78f3ec0a0277fcf045663148a6d0528e
Giulia <fioresegiulia@hotmail.com>
2020-03-25 16:40:10 +0100

    aggiunto file popolamento database

---
+++ Database/popolamento.sql
@@ -0,0 +1,5 @@
+/* popolamento tabella Utenti */
+INSERT INTO Utenti() VALUE
+('admin@admin.com', 'admin', 'Admin', 'Admin', NULL, NULL, NULL, 1),
+('utente@utente.com', 'admin', 'Utente', 'Utente', 999999999, 'Via Trieste, 63 - 35121 Padova (Italy)', '1970-01-01', 0);
+
