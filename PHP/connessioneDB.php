90e3c11eca4c8f48e9431cca067e90523771f865
Giulia <fioresegiulia@hotmail.com>
2020-03-28 11:19:43 +0100

    IMPORTANTE aggiornato file Generale dell'analisi

--- PHP/connessioneDB.php
+++ PHP/connessioneDB.php
@@ -1,35 +1,29 @@
 <?php
-class database_connection
-{
+class database_connection {
     private const HOST = 'localhost';
     private const USERNAME = 'admin';
     private const PASSWORD = 'admin';
     private const DATABASE_NAME = 'concessionaria';
     private $connessione;
 
-    
-    public function __construct()
-    {
+    public function __construct() {
         if (!($this->connessione = @mysqli_connect(static::HOST, static::USERNAME, static::PASSWORD, static::DATABASE_NAME))) {
             error_log("Debugging errno: " . mysqli_connect_errno()."Debugging error: " . mysqli_connect_error());
             echo "Momentaneamente i dati non sono disponibili. Riprovare più tardi.";
         }
     }
 
-    public function getConnessione()
-    {
+    public function getConnessione() { 
         return $this->getConnessione;
     }
 
-    public function esegui($query)
-    {
+    public function esegui($query) {
         $result = mysqli_query($this->connessione, $query);
         mysqli_close($this->connessione);
         return $result;
     }
 
-    public function chiudiConnessione()
-    {
+    public function chiudiConnessione() {
         mysqli_close($this->connessione);
     }
 }
