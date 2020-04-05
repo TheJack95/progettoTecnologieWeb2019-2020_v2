0660cf8eb2fb45bdf49828a1c54012e1f0b601a6
RiccardoConter <conte96@gmail.com>
2020-04-05 16:06:03 +0200

    fix logout admin

--- PHP/logout.php
+++ PHP/logout.php
@@ -6,6 +6,9 @@ try {
 		session_start();
 
 	unset($_SESSION["user"]);
+	unset($_SESSION["admin"]);
+	unset($_SESSION["utente"]);
+
 	$_SESSION["successmessage"] = "Logout effettuato con successo.";
 
 } catch (Exception $e) {
