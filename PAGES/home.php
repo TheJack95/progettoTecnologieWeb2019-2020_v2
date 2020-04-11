641930ab4cc696a055f9985317548769615528fe
TheJack95 <giacomocorro@gmail.com>
2020-04-11 16:17:29 +0200

    add css

--- PAGES/home.php
+++ PAGES/home.php
@@ -7,7 +7,7 @@
     $output = str_replace("<menu></menu>",funzioniGenerali::menu(),$output);
     $output = str_replace("<breadcrumb></breadcrumb>",funzioniGenerali::breadcrumb("Homepage"),$output);
     $output = str_replace("<footer></footer>",funzioniGenerali::footer(),$output);
-    $output = str_replace('<a href="home.php"><span xml:lang="en">HOME</span></a>','<strong>HOME</strong>',$output);
+    $output = str_replace('<a href="home.php"><span xml:lang="en" lang="en">HOME</span></a>','<strong>HOME</strong>',$output);
 
     echo $output;
 ?>
