28763622b7f60080edb042936de5d61ae1cbd967
Erik Tabacchi <tabacchi.erik@gmail.com>
2020-04-30 02:40:49 +0200

    add bottone scrollUp

--- JS/generale.js
+++ JS/generale.js
@@ -3,3 +3,19 @@ function toggleMenu(item) {
     let menu = document.getElementById('menu');
     menu.classList.toggle("menuMobile");
 }
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("scrollUp").classList.remove("hide");
+    }
+    else{
+        document.getElementById("scrollUp").classList.add("hide");
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0; 
+  }
