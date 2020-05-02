d9c51a1f77382e623499b6bfac8c38fe9d8da90b
RiccardoConter <conte96@gmail.com>
2020-05-02 11:47:34 +0200

    fix css e javascript

--- JS/controlliClient.js
+++ JS/controlliClient.js
@@ -1,44 +1,59 @@
-const form  = document.getElementsByTagName('form')[0];
-const email = document.getElementById('mail');
+//espressioni regolari
+const regex = [];
+regex["mail"] = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+regex["nome"] = /^[a-zA-Z]*$/;
+regex["cognome"] = /^[a-zA-Z]*$/;
+regex["messaggio"] = /^[a-zA-Z0-9]*$/;
+regex["numeroTelefono"] = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/;
+regex["password"] = /^[a-zA-Z0-9]+$/;
+regex["indirizzo"] = /^([a-zA-Z ]{3,11})\s([a-zA-Z ]+\s)+(\d{1,3}([\/][a-zA-Z ])?)$/;
+regex["nascita"] = /^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}$/;
+regex["data"] = /^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}$/;
+regex["prezzo"] = /^[[0-9]{1,2}(.)[0-9]{1,2}$/;
 
-const emailRegExp = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
-// Metodo Listener  per addEvent
-function addEvent(element, event, callback) {
-  let previousEventCallBack = element["on"+event];
-  element["on"+event] = function (e) {
-    const output = callback(e);
-    if (output === false) return false;
-    if (typeof previousEventCallBack === 'function') {
-      output = previousEventCallBack(e);
-      if(output === false) return false;
+var checkForm = (function(idForm) {
+    let text = "";
+    let elements = document.forms[idForm].getElementsByTagName("input");
+
+    let inputsKO = "";
+
+    for(let i = 0; elements && elements.length > i; ++i) {
+        if(elements[i].type != "submit" && elements[i].type != "file" && !validInput(elements[i], regex[elements[i].name] )) {
+            inputsKO += elements[i].name + ", ";
+            elements[i].className += " invalid";
+        }
     }
-  }
-};
 
-// Funzione di validazione
-addEvent(window, "load", function () {
-  const test = email.value.length === 0 || emailRegExp.test(email.value);
-  email.className = test ? "valid" : "invalid";
+    if(idForm == "registrationForm")
+        text += checkRegistrazione();
+
+    if(inputsKO.length) {
+        text += "Input " + inputsKO + " non valido/i";
+        alert(text);
+    }
+
+    return text.length == 0;
 });
 
-// Mentre l'utente digita
-addEvent(email, "input", function () {
-  const test = email.value.length === 0 || emailRegExp.test(email.value);
-  if (test) {
-    email.className = "valid";
-  } else {
-    email.className = "invalid";
-  }
-});
+function checkRegistrazione() {
+    let text = "";
 
-// Quando l'utente submitta
-addEvent(form, "submit", function () {
-  const test = email.value.length === 0 || emailRegExp.test(email.value);
-  if (!test) {
-    email.className = "invalid";
-    return false;
-  } else {
-    email.className = "valid";
-  }
-});
+    if(document.getElementById("password").value != document.getElementById("ripetiPassword").value) {
+        text += "Le password non coincidono. ";
+    }
+
+    let diff_ms = Date.now() - new Date(document.getElementById("nascita").value);
+    let age_dt = new Date(diff_ms);
+
+    if(Math.abs(age_dt.getUTCFullYear() - 1970 < 18)) {
+        text += "Devi essere maggiorenne per poterti iscrivere.";
+    }
+
+    return text;
+
+}
+
+function validInput(input, regex) {
+    return input.value != "" && regex.test(input.value)
+}
